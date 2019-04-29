b7d1904614e89c831aeacde15192b8d4297e6555
Henrique Cavalcante <henrique.cavalcante@yahoo.com.br>
2019-04-29 05:27:35 -0400

    Criação e configurações iniciais, new-entry, banco de dados sqlite e run device android

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,75 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { NavController } from "ionic-angular";
+import { NewEntryPage } from "../new-entry/new-entry";
+
+import { SQLite, SQLiteObject } from "@ionic-native/sqlite";
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  selector: "page-home",
+  templateUrl: "home.html"
 })
 export class HomePage {
+  constructor(public navCtrl: NavController, public sqlite: SQLite) {}
 
-  constructor(public navCtrl: NavController) {
-
+  addEntry() {
+    console.log("Adicionar Lançamento");
+    this.navCtrl.push(NewEntryPage); // abre uma tela
   }
 
+  testeDb() {
+    console.log("Início do Teste DB");
+
+    this.sqlite
+      .create({
+        name: "data.db",
+        location: "default"
+      })
+      .then((db: SQLiteObject) => {
+        console.log("DB criado");
+
+        db.sqlBatch([
+          [
+            "CREATE TABLE IF NOT EXISTS entries (id INTEGER PRIMARY KEY AUTOINCREMENT, amount DECIMAL, description TEXT)"
+          ]
+        ])
+          .then(() => {
+            console.log("Tabelas criadas");
+
+            const v1 = 100.2;
+            const v2 = "Teste 3";
+
+            const sqlInsert =
+              "INSERT INTO entries (amount, description) VALUES (?, ?)";
+            const dataInsert = [v1, v2];
+
+            db.executeSql(sqlInsert, dataInsert)
+              .then(() => {
+                console.log("Valores inseridos");
+
+                const sql = "SELECT amount, description FROM entries;";
+                const data = [];
+
+                db.executeSql(sql, data).then((values: any) => {
+                  console.log(values.rows.length);
+
+                  for (var i = 0; i < values.rows.length; i++) {
+                    console.log(JSON.stringify(values.rows.item(i)));
+                  }
+                });
+              })
+              .catch(e => {
+                console.error(
+                  "erro ao inserir os valores no banco de dados",
+                  JSON.stringify(e)
+                );
+              });
+          })
+          .catch(e => {
+            console.error("erro ao executar o comando SQL", JSON.stringify(e));
+          });
+      })
+      .catch(() => {
+        console.error("Erro ao criar o BD.");
+      });
+  }
 }
---
+++ src/pages/new-entry/new-entry.ts
@@ -0,0 +1,31 @@
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+
+@IonicPage()
+@Component({
+  selector: "page-new-entry",
+  templateUrl: "new-entry.html"
+})
+export class NewEntryPage {
+  entry = {
+    value: "0,00",
+    category: 1
+  };
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  ionViewDidLoad() {
+    console.log("ionViewDidLoad NewEntryPage");
+  }
+
+  submitForm() {
+    console.log("Submit form");
+    console.log(this.entry);
+    this.goBack();
+  }
+
+  goBack() {
+    console.log("GoBack");
+    this.navCtrl.pop(); //fecha a tela
+  }
+}
