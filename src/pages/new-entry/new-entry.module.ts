72604321ea0f9d7999839b435f894b778853f8cf
Henrique Cavalcante <henrique.cavalcante@yahoo.com.br>
2019-04-30 22:43:50 -0400

    Alteração no new-entry para salvar no banco de dados e uso da lib currency-mask

--- src/pages/new-entry/new-entry.module.ts
+++ src/pages/new-entry/new-entry.module.ts
@@ -1,13 +1,12 @@
-import { NgModule } from '@angular/core';
-import { IonicPageModule } from 'ionic-angular';
-import { NewEntryPage } from './new-entry';
+import { NgModule } from "@angular/core";
+import { IonicPageModule } from "ionic-angular";
+
+import { CurrencyMaskModule } from "ng2-currency-mask";
+
+import { NewEntryPage } from "./new-entry";
 
 @NgModule({
-  declarations: [
-    NewEntryPage,
-  ],
-  imports: [
-    IonicPageModule.forChild(NewEntryPage),
-  ],
+  declarations: [NewEntryPage],
+  imports: [IonicPageModule.forChild(NewEntryPage), CurrencyMaskModule]
 })
 export class NewEntryPageModule {}
--- src/pages/new-entry/new-entry.ts
+++ src/pages/new-entry/new-entry.ts
@@ -1,5 +1,13 @@
 import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder
+} from "@angular/forms";
+
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
 
 @IonicPage()
 @Component({
@@ -7,25 +15,70 @@ import { IonicPage, NavController, NavParams } from "ionic-angular";
   templateUrl: "new-entry.html"
 })
 export class NewEntryPage {
-  entry = {
-    value: "0,00",
-    category: 1
-  };
+  entryForm: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+  entry = { };
 
-  ionViewDidLoad() {
-    console.log("ionViewDidLoad NewEntryPage");
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private builder: FormBuilder,
+    public sqlite: SQLite
+  ) {
+    this.entryForm = builder.group({
+      amount: new FormControl(
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.pattern("[0-9,.]+")
+        ])
+      ),
+      category_id: new FormControl("", Validators.required)
+    });
   }
 
+  ionViewDidLoad() {}
+
   submitForm() {
-    console.log("Submit form");
-    console.log(this.entry);
-    this.goBack();
+    console.log("Enviando dados...");
+    console.log(JSON.stringify(this.entry));
+
+    this.insertBD();
+
+    this.navCtrl.pop();
   }
 
   goBack() {
-    console.log("GoBack");
+    console.log("Cancelando...");
     this.navCtrl.pop(); //fecha a tela
+  }
+
+  insertBD(){
+    console.log('Início da gravação do BD');
+
+    this.sqlite.create({
+      name: 'data.db',
+      location: 'default'
+    })
+    .then((db: SQLiteObject) => {
+      console.log('bd criado');
+
+      db.sqlBatch([
+        "CREATE TABLE IF NOT EXISTS entries (id INTEGER PRIMARY KEY AUTOINCREMENT, amount DECIMAL, description TEXT)"
+      ])
+      .then(() => {
+
+        const sql = "INSERT INTO entries (amount) VALUES (?)";
+        const data = [this.entry['amount']];
+    
+        return db
+          .executeSql(sql, data)
+          .then(() => console.log('insert realizado com sucesso'))
+          .catch(e => console.error("erro ao inserir na tabela", JSON.stringify(e)));
+        
+      })
+      .catch(e => console.error("erro ao criar a tabela", JSON.stringify(e)));
+
+    });
   }
 }
